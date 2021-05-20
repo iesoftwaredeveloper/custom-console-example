6a0fb6686641be6a1382c973b4f0ae8102d9912f
Robert Little <rll04747@pomona.edu>
2021-05-20 11:47:33 -0700

    Implement CustomConsole

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.CommandLine;
+using System.CommandLine.IO;
+using System.IO;
 
 namespace custom_console_example
 {
@@ -6,7 +9,24 @@ namespace custom_console_example
     {
         static void Main(string[] args)
         {
+            MemoryStream ms = new MemoryStream();
+            TextWriter tw = new StreamWriter(ms);
+
+            IConsole console = new CustomConsole(StandardStreamWriter.Create(tw));
+
+            var rootCommand = new RootCommand()
+            {
+                Description = "Console app to demonstrate System.CommandLine.IConsole"
+            };
+
             Console.WriteLine("Hello World!");
+
+            rootCommand.InvokeAsync(args, console).Wait();
+
+            tw.Flush();
+            ms.Position = 0;
+
+            Console.Write(new StreamReader(ms).ReadToEnd());
         }
     }
 }
